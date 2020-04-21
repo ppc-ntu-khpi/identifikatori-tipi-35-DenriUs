d28ba656e88cfe2b4333c34a346457eeb6a8baee
DenriUs <den.tacij@gmail.com>
2020-04-21 16:00:22 +0300

    Add Caesars encryption functionality

--- src/domain/Exercise.java
+++ src/domain/Exercise.java
@@ -1,7 +1,19 @@
-package domain;
+package domain; 
+ 
+public class Exercise { 
+    /**
+     * Static method that encrypt input string with Caesar's ecnryption
+     * @param Input string to encrypt
+     * @param DisplacementVaule number of encryption displacements
+     * @return encrypted string
+     */
+    public static String Calculate(final String Input, final int DisplacementVaule) {
+        StringBuilder result = new StringBuilder();
 
-public class Exercise {
-    public static float Calculate(int R){
-        return (float) (2*Math.PI*Math.sqrt(R));
-    }
-}
+        for (char item : Input.toCharArray()) {
+            result.append((char)((int)item + DisplacementVaule));
+        }
+
+        return result.toString();
+    } 
+} 
--- src/test/TestResult.java
+++ src/test/TestResult.java
@@ -1,12 +1,35 @@
 package test;
 
 import domain.Exercise;
+import java.util.Scanner;
 
 public class TestResult {
-
     public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
 
-        int R=10;
-        System.out.println("Area of a circle for R="+R+"m is "+Exercise.Calculate(R)+"m"+'\u00B2');
+        int displacementValue;
+        String inputString;
+
+        System.out.println("Enter string to encrypt: ");
+        inputString = keyboard.nextLine();
+
+        do {
+            System.out.println("String to encrypt: " + inputString);
+            System.out.println("\nEnter a numeric value of the displacement: ");
+
+            while (!keyboard.hasNextInt()) {
+                String input = keyboard.nextLine();
+
+                System.out.println("'" + input +"'" + " is not a number");
+                System.out.println("String to encrypt: " + inputString);
+                System.out.println("\nEnter a numeric value of the displacement: ");
+            }
+
+            displacementValue = keyboard.nextInt();
+        } while (displacementValue < 0);
+
+        keyboard.close();
+
+        System.out.println("Result: " + Exercise.Calculate(inputString, displacementValue));
     }
 }
